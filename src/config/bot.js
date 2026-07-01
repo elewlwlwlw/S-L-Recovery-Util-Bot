b9eddc5a9a1cb468334f9452143ebb501e9b74a8
elewlwlwlw <ellewell23@bi.kgapupils.uk>
2026-07-01 17:47:55 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "24/7 S&L Recovery Bot!",
         // Activity type number (0 = Playing).
         type: 0,
       },
